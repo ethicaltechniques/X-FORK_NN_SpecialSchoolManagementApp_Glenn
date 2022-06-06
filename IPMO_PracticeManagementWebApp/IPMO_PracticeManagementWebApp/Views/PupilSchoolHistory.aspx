facdcf2f2a1537e4501a676733c73e8c38d3e345
Sudipta Chakraborty <sudipta89chakraborty@gmail.com>
2022-06-07 01:37:39 +0530

    dynamic textbox

--- IPMO_PracticeManagementWebApp/IPMO_PracticeManagementWebApp/Views/PupilSchoolHistory.aspx
+++ IPMO_PracticeManagementWebApp/IPMO_PracticeManagementWebApp/Views/PupilSchoolHistory.aspx
@@ -1,32 +1,49 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="PupilSchoolHistory.aspx.cs" Inherits="IPMO_PracticeManagementWebApp.Views.PupilSchoolHistory" %>
 
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
     <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <asp:PlaceHolder ID="PupilSchoolHistoryPlaceHolder" runat="server">
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <asp:Panel ID="ValidationPanelForPupilSchoolHistory" runat="server">
+            </asp:Panel>
+
+            <p>
+                <h1>2a Form for Pupil School History</h1>
+                <asp:Button ID="AttachmentButton" runat="server" Text="+Attachment" />
+            </p>
+            <br />
+
+            <p>
+                <h5>
+                    <asp:TextBox ID="PassportNumberLabel" runat="server" Text="Passport Number"></asp:TextBox>
+                </h5>
+            </p>
+            <p>
+                <asp:TextBox ID="PassportNumberTextBox" runat="server" TextMode="MultiLine"></asp:TextBox>
+
+            </p>
+
+            <asp:Table ID="table" runat="server"></asp:Table>
+
+        </asp:PlaceHolder>
 
         <p>
-            <h1>2a Form for Pupil School History</h1>
-            <asp:Button ID="AttachmentButton" runat="server" Text="+Attachment" />
+            <asp:Button ID="FieldsButton" runat="server" Text="+fields" OnClick="AddFieldButton_Click" />
         </p>
-        <br />
 
         <p>
-            <h5>Primary Education</h5>
-            <textarea id="PrimaryEducationTextArea" cols="20" rows="2"></textarea>
-        </p>
-        <p>
-            <h5>Secondary Education</h5>
-            <textarea id="SecondaryEducationTextArea" cols="20" rows="2"></textarea>
-        </p>
-        <p>
-            <asp:Button ID="FieldsButton" runat="server" Text="+fields" />
+            <asp:Button ID="PupilSchoolHistoryButton" runat="server" Text="Submit Pupil School History" OnClick="PupilSchoolHistoryButton_Click" />
         </p>
     </div>
 
+    <%--</asp:PlaceHolder>--%>
 </asp:Content>
--- IPMO_PracticeManagementWebApp/IPMO_PracticeManagementWebApp/Views/PupilSchoolHistory.aspx.designer.cs
+++ IPMO_PracticeManagementWebApp/IPMO_PracticeManagementWebApp/Views/PupilSchoolHistory.aspx.designer.cs
@@ -15,6 +15,24 @@ namespace IPMO_PracticeManagementWebApp.Views
     {
 
         /// <summary>
+        /// PupilSchoolHistoryPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder PupilSchoolHistoryPlaceHolder;
+
+        /// <summary>
+        /// ValidationPanelForPupilSchoolHistory control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel ValidationPanelForPupilSchoolHistory;
+
+        /// <summary>
         /// AttachmentButton control.
         /// </summary>
         /// <remarks>
@@ -24,6 +42,33 @@ namespace IPMO_PracticeManagementWebApp.Views
         protected global::System.Web.UI.WebControls.Button AttachmentButton;
 
         /// <summary>
+        /// PassportNumberLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PassportNumberLabel;
+
+        /// <summary>
+        /// PassportNumberTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PassportNumberTextBox;
+
+        /// <summary>
+        /// table control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table table;
+
+        /// <summary>
         /// FieldsButton control.
         /// </summary>
         /// <remarks>
@@ -31,5 +76,14 @@ namespace IPMO_PracticeManagementWebApp.Views
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button FieldsButton;
+
+        /// <summary>
+        /// PupilSchoolHistoryButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button PupilSchoolHistoryButton;
     }
 }
